e709a0944eefac80da093b13c8c1c6c1726b3ec6
Raiosilva <raimundopi025@gmail.com>
2020-06-02 22:06:17 -0300

    auth login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,13 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+
+import { JwtHelper } from 'angular2-jwt';
+
+import { CredentialsDTO } from './../models/credentials.dto';
 import { StorageService } from './storage.service';
 import { LocalUser } from './../models/local_user';
 import { API_CONFIG } from './../config/api.config';
-import { HttpClient } from '@angular/common/http';
-import { CredentialsDTO } from './../models/credentials.dto';
-import { Injectable } from "@angular/core";
-
 
 @Injectable()
 export class AuthService {
+
+  jwtHelper: JwtHelper = new JwtHelper();
 
   constructor(
     public http: HttpClient,
@@ -28,7 +32,8 @@ export class AuthService {
   successfulLogin(authorizationValue:string) {
     let tok = authorizationValue.substring(7);
     let user: LocalUser = {
-      token: tok
+      token: tok,
+      email: this.jwtHelper.decodeToken(tok).sub
     };
     this.storageService.setLocalUser(user);
   }
