16a52b6839727968b9512c9ddd63343b8611cc34
Raiosilva <raimundopi025@gmail.com>
2020-06-15 16:57:49 -0300

    adding image from products

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,7 +1,10 @@
-import { ProductService } from './../../services/domain/product.service';
-import { ProductDTO } from './../../models/product.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { API_CONFIG } from './../../config/api.config';
+
+import { ProductService } from './../../services/domain/product.service';
+import { ProductDTO } from './../../models/product.dto';
 
 @IonicPage()
 @Component({
@@ -23,8 +26,21 @@ export class ProductsPage {
     let categoria_id = this.navParams.get('categoria_id');
     this.productService.findByCategory(categoria_id).subscribe(response => {
       this.items = response['content'];
+      this.loadImageUrls();
     },
     error => {}
     );
   };
+
+  loadImageUrls() {
+    for (let i = 0; i < this.items.length; i++) {
+      let item = this.items[i];
+
+      this.productService.getSmallImageFromBucket(item.id).subscribe(response => {
+        item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+      },
+      error => {}
+      );
+    }
+  }
 }
