cc561bbda918b8ab2dd7a38e6f476559f7287d1b
Raiosilva <raimundopi025@gmail.com>
2020-06-19 14:46:45 -0300

    adding methods to load of image

--- src/pages/product-detail/product-detail.ts
+++ src/pages/product-detail/product-detail.ts
@@ -1,6 +1,8 @@
+import { ProductService } from './../../services/domain/product.service';
 import { ProductDTO } from './../../models/product.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -11,15 +13,30 @@ export class ProductDetailPage {
 
   item: ProductDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public productService: ProductService
+    ) {
   }
 
   ionViewDidLoad() {
-    this.item = {
-      id: '1',
-      nome: 'Mouse',
-      preco: 80.59
-    }
+    let produto_id = this.navParams.get('produto_id');
+    this.productService.findById(produto_id).subscribe(response => {
+      this.item = response;
+      this.getImageUrlExists();
+    },
+    error => {
+
+    });
   }
 
+  getImageUrlExists() {
+    this.productService.getImageFromBucket(this.item.id).subscribe(response => {
+      this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`
+    },
+    error => {
+
+    });
+  }
 }
--- src/services/domain/product.service.ts
+++ src/services/domain/product.service.ts
@@ -1,3 +1,4 @@
+import { ProductDTO } from './../../models/product.dto';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 
@@ -10,12 +11,21 @@ export class ProductService {
 
   constructor(public http: HttpClient) {}
 
+  findById(produto_id: string) {
+    return this.http.get<ProductDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+  }
+
   findByCategory(categoria_id: string) {
     return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
   }
 
   getSmallImageFromBucket(id: string): Observable<any> {
     let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+    return this.http.get(url, {responseType: 'blob'});
+  }
+
+  getImageFromBucket(id: string): Observable<any> {
+    let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
     return this.http.get(url, {responseType: 'blob'});
   }
 }
